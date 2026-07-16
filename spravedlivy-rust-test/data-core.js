459e8606461bec98a878c87ff5349b8fbd7dc777
vrnak-007 <vrnak@ksco.cz>
2026-07-16 20:13:29 +0200

    Add quiz data core

---
+++ spravedlivy-rust-test/data-core.js
@@ -0,0 +1,4 @@
+const DIMS = ["lr","redist","growth","integrity","target","coverage","time","econFreedom","civicFreedom","admin","fiscal","family"];
+const S = (lr,redist,growth,integrity,target,coverage,time,econFreedom,civicFreedom,admin,fiscal,family) => ({lr,redist,growth,integrity,target,coverage,time,econFreedom,civicFreedom,admin,fiscal,family});
+
+const questions = [];
